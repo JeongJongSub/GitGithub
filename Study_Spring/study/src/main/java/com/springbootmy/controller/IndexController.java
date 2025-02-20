325373b3e57b28acc0ab5e0a6e1519030826309c
JeongJongSub <jeongjs501@gmail.com>
2025-02-20 17:41:46 +0900

    스프링부터 vsc에 적용해보기 성공공

---
+++ Study_Spring/study/src/main/java/com/springbootmy/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.springbootmy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("message", "Hello Spring Boot!");
+    model.addAttribute("checked", true);
+    return "index";
+  }
+  
+}
